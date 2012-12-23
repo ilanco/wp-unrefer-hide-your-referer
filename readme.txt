5e6e860c30acd6d711f56ebfaa8bef81bd37e409
Ilan Cohen <ilanco@gmail.com>
2012-12-23 00:53:31 +0000

    initial commit

---
+++ readme.txt
@@ -0,0 +1,42 @@
+=== WordPress Hide Referer ===
+Contributors: ilanco
+Donate link: https://www.gittip.com/ilanco/
+Tags: hide, refer, referer, referrer, hide refer, hide referer, hide referrer, anonymize, anonymize link, privacy
+Requires at least: 3.3
+Tested up to: 3.5
+Stable tag: 1.0
+License: WTFPL
+License URI: http://sam.zoy.org/wtfpl/
+
+Hide your referer (referrer) on all external links with unrefer.com.
+
+== Description ==
+
+Hide Referer for WordPress removes the referer (referrer) from all external links on your blog. It does this by converting all outgoing 
+links on your blog using the unrefer.com service and effectively hides your site as origin of the referred link.
+
+For example, Hide Referer for WordPress will convert an external link to wordpress.com, to [http://unrefer.com/?http://wordpress.org](http://unrefer.com/?http://wordpress.org).
+
+== Installation ==
+
+1. Install WordPress Hide Referer either via the WordPress.org plugin directory, or by uploading the files to your server.
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. All your external links are un-refered
+
+== Frequently Asked Questions ==
+
+= Is this free? =
+
+Yes, this plugin and the unrefer.com service are free to use.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
+
+== Upgrade Notice ==
+
+== Screenshots ==
+
+No screenshots yet.
+
---
+++ wp-unrefer-hide-your-referer.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Plugin Name: WordPress Hide Referer
+ * Plugin URI: https://github.com/ilanco/wp-unrefer-hide-your-referer
+ * Description: Hide your referer.
+ * Version: 1.0
+ * Author: Ilan Cohen <ilanco@gmail.com>
+ * Author URI: https://github.com/ilanco
+ */
+
+/**
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details.
+ */ 
+
+if (defined('WP_DEBUG') && (WP_DEBUG == true)) {
+  error_reporting(E_ALL);
+}
+
+// don't load directly
+if (!defined('ABSPATH'))
+  die(false);
+
+add_action('init', array('WPHideReferer', 'init'), 9);
+
+class WPHideReferer
+{
+    private static $instance;
+
+    private function __construct()
+    {
+        // filter content text
+        add_filter('the_content', array($this, 'convert'), 999);
+
+        // filter comment text
+        add_filter('comment_text', array($this, 'convert'), 999);
+    }
+
+    public static function init()
+    {
+        if (!self::$instance) {
+            self::$instance = new WPHideReferer();
+        }
+
+        return self::$instance;
+    }
+
+    public function convert($content)
+    {
+        // check for links in content
+        if (stripos($content, 'href=') === false) {
+            return $content;
+        }
+
+        $urls = $this->domDocumentExtract($content);
+
+        $urls = $this->regexExtract($content);
+
+        // remove duplicate urls
+        $urls = array_unique($urls);
+
+        // check if urls are valid to unrefer
+        $urls = array_filter($urls, array($this, 'validate'));
+
+        $converted_urls = array_map(array($this, 'prefix'), $urls);
+
+        $content = str_ireplace($urls, $converted_urls, $content);
+
+        return $content;
+    }
+
+    private function validate($url)
+    {
+        return (stripos($url, 'http') === 0);
+    }
+
+    private function prefix($url)
+    {
+        return 'http://unrefer.com/?' . $url;
+    }
+
+    private function domDocumentExtract($content)
+    {
+        $urls = array();
+
+        $lui_errors = libxml_use_internal_errors(true);
+
+        $dom = new DOMDocument();
+        $dom->strictErrorChecking = false;
+        $dom->loadHTML($content);
+
+
+        $xPath = new DOMXPath($dom);
+        $nodes = $xPath->query('//a/@href');
+
+        foreach ($nodes as $href) {
+            $urls[] = $href->nodeValue;
+        }
+
+        unset($nodes);
+        unset($xPath);
+        unset($dom);
+
+        libxml_clear_errors();
+        libxml_use_internal_errors($lui_errors);
+
+        return $urls;
+    }
+
+    private function regexExtract($content)
+    {
+        $urls = array();
+
+        $regex = '/(<a\s*'; // Start of anchor tag
+        $regex .= '(.*?)\s*'; // Any attributes or spaces that may or may not exist
+        $regex .= 'href=[\'"]+?\s*(?P<link>\S+)\s*[\'"]+?'; // Grab the link
+        $regex .= '\s*(.*?)\s*>\s*'; // Any attributes or spaces that may or may not exist before closing tag
+        $regex .= '(?P<name>[^<]*)'; // Grab the name
+        $regex .= '\s*<\/a>)/i'; // Any number of spaces between the closing anchor tag (case insensitive)
+   
+        if (preg_match_all($regex, $content, $matches) !== false) {
+            $urls = $matches['link'];
+        }
+
+        return $urls;
+    }
+}
+
